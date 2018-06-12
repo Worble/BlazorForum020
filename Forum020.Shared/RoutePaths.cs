f7d1668288f0a0fb078516f5ffd93cac50a68d89
Robert Clarke <Robert.Clarke@gocompare.com>
2018-06-12 19:03:20 +0100

    changed api path

--- Forum020.Shared/RoutePaths.cs
+++ Forum020.Shared/RoutePaths.cs
@@ -2,7 +2,7 @@
 {
     public static class RoutePaths
     {
-        public static string Api => "http://localhost:8607/api/";
+        public static string Api => "http://localhost:8610/api/";
         public static string Boards() => "/";
         public static string Threads(string Board) => "/" + Board;
         public static string Posts(string Board, int Thread) => "/" + Board + "/" + Thread;
