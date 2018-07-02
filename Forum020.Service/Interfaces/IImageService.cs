c4535298a20abd4a0dcabbd5e87a7d6eae5806ff
Robert Clarke <Robert.Clarke@gocompare.com>
2018-07-02 16:01:06 +0100

    added from wrong directory

--- Forum020.Service/Interfaces/IImageService.cs
+++ Forum020.Service/Interfaces/IImageService.cs
@@ -1,9 +1,4 @@
 ﻿using Forum020.Shared;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Forum020.Service.Interfaces
